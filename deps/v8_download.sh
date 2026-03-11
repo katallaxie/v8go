fd600550d980f4a2d26aee58eb409e26d9b688ca
Sebastian Döll <sebastian@katallaxie.dev>
2026-03-11 22:15:51 +0000

    chore: force update tools

--- deps/v8_download.sh
+++ deps/v8_download.sh
@@ -7,7 +7,7 @@ dir="$(cd "$(dirname "$0")" && pwd)"
 PATH="${dir}/depot_tools:$PATH"
 export PATH
 
-# export DEPOT_TOOLS_UPDATE=1
+export DEPOT_TOOLS_UPDATE=1
 
 version="$(head -n1 "${dir}/VERSION" | cut -d'-' -f1)"
 
